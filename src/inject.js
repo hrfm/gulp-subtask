f31c9931dcc2bf9000cf92b9f73503e71b307294
KAWAKITA Hirofumi <hrfm0818@gmail.com>
2015-01-11 20:55:56 +0900

    inject.js bug fix

--- src/inject.js
+++ src/inject.js
@@ -4,34 +4,37 @@
 
   module.exports = function( target, src ){
 
+    var obj;
     var type = typeof target;
 
     if( !src || type === 'undefined' || type === 'function' ){
       return target;
     }
-
+    
     if( type === 'string' ){
 
       // --- String.
 
-      if( /\{\{(\w+)\}\}/.test(target) ){
+      obj = target;
+
+      if( /\{\{(\w+)\}\}/.test(obj) ){
 
         var tags = target.match(/\{\{(\w+)\}\}/g);
 
         if( tags && 0 < tags.length ){
 
-          if( tags.length == 1 && tags[0] === target ){
+          if( tags.length == 1 && tags[0] === obj ){
 
             // If tags.length is one and target string is completely same as tags[0].
             // Only this condition. Target can replaced with src.value. 
             var result = (/\{\{(\w+)\}\}/).exec( tags[0] );
-            target = arguments.callee( src[result[1]], src );
+            obj = arguments.callee( src[result[1]], src );
             
           }else{
 
             for( var i=0; i<tags.length; i++ ){
               var result = /\{\{(\w+)\}\}/.exec(tags[i]);
-              target = target.replace( result[0], src[result[1]].toString() );
+              obj = obj.replace( result[0], src[result[1]].toString() );
             }
 
           }
@@ -43,23 +46,24 @@
     }else if( target instanceof Array ){
       
       // --- Array.
-      
+      obj = [];
       for( var i = 0; i < target.length; i++ ){
-        target[i] = arguments.callee( target[i], src );
+        obj[i] = arguments.callee( target[i], src );
       }
 
     }else if( type === 'object' ){
       
       // --- Object.
       
+      obj = {};
       for( var key in target ){
-        target[key] = arguments.callee( target[key], src );
+        obj[key] = arguments.callee( target[key], src );
       }
 
     }
     
-    return target;
-
+    return obj;
+    
   }
 
 }).call(this);
